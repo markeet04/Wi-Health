e9a9b4f6a4405a95f0101f646aa56fb039161328
bilaladnan05 <m.bilal.adnan05@gmail.com>
2026-08-01 21:29:23 +0500

    qasim ka shit loading page refine kar dia

--- mobile/lib/screens/splash_screen.dart
+++ mobile/lib/screens/splash_screen.dart
@@ -59,6 +59,8 @@ class _SplashScreenState extends State<SplashScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: double.infinity,
+        height: double.infinity,
         decoration: const BoxDecoration(gradient: wiSkyGradient),
         child: SafeArea(
           child: Column(
